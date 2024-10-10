2bbe586bd978bf4cef26ec74e5be949fa527936d
JoanAmaya <98501917+JoanAmaya@users.noreply.github.com>
2024-10-09 23:33:50 -0500

    Update urls.py

--- whiletrue/whiletrue/urls.py
+++ whiletrue/whiletrue/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reportes-csv/', include('reportes.urls'))
+    path('reportes-csv/', include('reportes.urls')),
+    path('health-check/', views.healthCheck),
 ]
