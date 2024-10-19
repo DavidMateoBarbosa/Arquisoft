88f180ee91e948479149ffe94efd1d058993786b
LaraGomezN <111064261+LaraGomezN@users.noreply.github.com>
2024-10-18 21:34:04 -0500

    migrations

--- whiletrue/usuarios/models.py
+++ whiletrue/usuarios/models.py
@@ -1,6 +1,16 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
+import hashlib
 
-class Usuario(AbstractUser): 
-    def __str__(self) -> str:
-        return f'User(username={self.username}, isadmin={self.is_staff}, active={self.is_active})'
+from django.contrib.auth.hashers import make_password
+from django.db import models
+
+class Usuario(models.Model): 
+    username: str = models.CharField(max_length=255)
+    password: str = models.CharField(max_length=255)
+    email: str = models.EmailField()
+    firstname: str = models.CharField(max_length=50)
+    lastname: str = models.CharField(max_length=50)
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.password.startswith('pbkdf2_'):
+            self.password = make_password(hashlib.sha512(self.password.encode()).hexdigest())
+        super().save(*args, **kwargs)
