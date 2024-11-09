5654846ab17134772367ea01918c8465b380c6b1
JoanAmaya <98501917+JoanAmaya@users.noreply.github.com>
2024-11-08 20:26:06 -0500

    Update views.py health fix

--- whiletrue/whiletrue/views.py
+++ whiletrue/whiletrue/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.http import HttpResponse
 
 def healthCheck(request):
     return HttpResponse('ok')
+
+def health_check(request):
+    return JsonResponse({'message': 'OK'}, status=200)
