89f67b5db0dc6772fba64a10a6e8f20adecaf3c6
Joan Amaya <j.amayab@uniandes.edu.co>
2024-12-05 23:36:17 -0500

    Cambios mongodb

--- whiletrue/whiletrue/settings.py
+++ whiletrue/whiletrue/settings.py
@@ -78,16 +78,19 @@ WSGI_APPLICATION = 'whiletrue.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+
+
+
 DATABASES = {
-    'default': {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "reportdb",
-        "USER": "admin",
-        "PASSWORD": "root",
-        "HOST": "localhost",
-        "PORT": "5432",
-        
-    }
+        'default': {
+            'ENGINE': 'djongo',
+            'NAME': 'reportdb',
+            'ENFORCE_SCHEMA': False,
+            'CLIENT': {
+                 'host': 'mongodb://localhost:27017/'
+             }  
+        }
 }
 
 
